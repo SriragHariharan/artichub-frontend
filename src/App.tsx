a0f06b528c393bfb52292197e83fd45e2e08c76a
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-04-14 16:31:31 +0530

    Created basic routes using react-router package.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import CreateArticle from './pages/CreateArticle';
+import SettingsPage from './pages/SettingsPage';
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/create" element={<CreateArticle/>} />
+        <Route path="/account" element={<SettingsPage/>} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
